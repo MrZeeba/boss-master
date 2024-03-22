65d0c26faa704d2d6916c00cc50129a4743950b9
Zeeba <Zeeba@STARK REBORN>
2024-03-22 15:31:38 +0000

    Some basic icons for the nav bar

--- src/navigation/NavigationBar.tsx
+++ src/navigation/NavigationBar.tsx
@@ -1,18 +1,56 @@
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import EditShootSession from '../pages/EditShootSession';
 import History from '../pages/History';
 import Profile from '../pages/Profile';
 import Settings from '../pages/Settings';
+Feather;
 
 const Tab = createBottomTabNavigator();
 
 export default function NavigationBar() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Shoot" component={EditShootSession} />
-      <Tab.Screen name="History" component={History} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Shoot"
+        component={EditShootSession}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="target" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={History}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="notebook-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
