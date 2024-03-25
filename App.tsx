b9dcd564cfe9352404c4b39eec84535ef772a7a8
Zeeba <Zeeba@STARK REBORN>
2024-03-25 16:49:24 +0000

    wip

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './src/pages/SignUp';
 
 export default function App() {
   console.log('I am a line of logging!!');
 
   return (
     <View style={styles.container}>
+      <SignUp />
       <Text>This text has been updated! Or has it?</Text>
       <StatusBar style="auto" />
     </View>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,9 @@
 import { Button } from 'react-native';
-import { register } from '../firebase/auth';
 
 export default function SignUp() {
   return (
     <Button
-      onPress={register('foo@gmail.com', 'testpassword123')}
+      onPress={() => console.log('I was pressed')}
       title="Submit Details"
     />
   );
