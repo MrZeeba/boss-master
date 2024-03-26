4e9591c3b602ca041d9dc944b8febc65ce866b55
Zeeba <Zeeba@STARK REBORN>
2024-03-26 09:39:28 +0000

    Add form

--- docs/ui-libraries.md
+++ docs/ui-libraries.md
@@ -9,3 +9,15 @@ This app utilises various 3rd party libraries to save time. You'll find a list h
 ## [Expo Vector Icons](https://icons.expo.fyi/Index)
 
 - Primarily this is where I get icons for the app
+
+## [React Hook Form](https://react-hook-form.com/)
+
+Provides functionality to submit forms
+
+- New user
+- New session
+
+## [React Native Picker] (https://github.com/react-native-picker/picker?tab=readme-ov-file)
+
+[Expo Documentation](https://docs.expo.dev/versions/latest/sdk/picker/)
+A library containing a picker component
--- src/pages/EditShootSession.tsx
+++ src/pages/EditShootSession.tsx
@@ -1,29 +1,60 @@
 import { addDoc, collection } from 'firebase/firestore';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
+import { Button, Text, TextInput, View } from 'react-native';
 import { db } from '../firebase/firebaseApp';
 
+type ShootSessionData = {
+  type: string;
+  note: string;
+  bow: string;
+  date: string;
+};
+
 export default function EditShootSession() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    const date = new Date();
+    data.date = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    console.log('Submitted Data', data);
+    //await AddSession(data);
+  };
+
   return (
     <View>
-      <Text style={styles.baseText}>
-        I am bold
-        <Text style={styles.innerText}> and red</Text>
-      </Text>
-      <Button onPress={AddSession} title="Add temp session data" />
+      <Controller
+        control={control}
+        rules={{
+          required: 'You must enter a value',
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="my foo placeholder"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="Foo"
+      />
+      {errors.type && <Text>This is required.</Text>}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
 
-async function AddSession() {
+async function AddSession(sessionData: ShootSessionData) {
   try {
-    const date = new Date();
-    const currentDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
-
     const docRef = await addDoc(collection(db, 'shooting_sessions'), {
-      type: 'Practice',
-      bow: 'Recurve',
-      date: currentDate,
+      type: sessionData.type,
+      bow: sessionData.bow,
+      date: sessionData.date,
+      note: sessionData.note,
     });
 
     console.log('Document written with ID: ', docRef.id);
@@ -31,12 +62,3 @@ async function AddSession() {
     console.error('Error adding document: ', e);
   }
 }
-
-const styles = StyleSheet.create({
-  baseText: {
-    fontWeight: 'bold',
-  },
-  innerText: {
-    color: 'red',
-  },
-});
