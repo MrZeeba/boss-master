7fca4d4a030b9836bd76e2ce87f1d913990b9a46
Zeeba <Zeeba@STARK REBORN>
2024-03-28 13:58:50 +0000

    WIP Unit tests for sqlite database

--- src/sqlite/LocalDb.test.tsx
+++ src/sqlite/LocalDb.test.tsx
@@ -1,8 +1,35 @@
 //Mock the database
+import * as SQLite from 'expo-sqlite';
+import { localDB } from './localdb';
+
+jest.mock('expo-sqlite', () => ({
+  openDatabase: jest.fn(),
+}));
+
+// Mock the module
+jest.mock('expo-sqlite', () => ({
+  openDatabase: jest.fn(),
+}));
+
 describe('Connect to local database', () => {
-  it('open a new connection', () => {
-    expect(1).toBe(2);
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset mocks after each test case
   });
 
-  it('get an existing connection', () => {});
+  it('open a new connection', () => {
+    localDB();
+    expect(SQLite.openDatabase).toHaveBeenCalledWith('boss-master.db');
+  });
+
+  it('get an existing connection', () => {
+    // Mock an existing connection
+    const mockConnection = { mock: 'connection' };
+    SQLite.openDatabase.mockImplementation(mockConnection); // Mock the return value of openDatabase
+
+    // Call localDB function
+    const connection = localDB();
+
+    // Assert that it returns the existing connection
+    expect(connection).toEqual(mockConnection);
+  });
 });
