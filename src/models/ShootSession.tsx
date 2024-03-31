10a8552458b1344514a66201be8f1dade09fdefd
Zeeba <Zeeba@STARK REBORN>
2024-03-31 19:18:09 +0100

    Method to get all sessions (WIP)

--- src/models/ShootSession.tsx
+++ src/models/ShootSession.tsx
@@ -1,6 +1,7 @@
 import { Bow } from './Bow';
 
 export class ShootSession {
+  id: number | undefined;
   date: string;
   bow: Bow;
   note: string;
--- src/sqlite/LocalDb.tsx
+++ src/sqlite/LocalDb.tsx
@@ -3,6 +3,7 @@ import { Platform } from 'react-native';
 import { ShootSession } from '../models/ShootSession';
 
 let _localDB: SQLite.Database;
+const shootSessionTableName = `shootsessions`;
 
 /*
 Return an open connection
@@ -54,25 +55,44 @@ export function AddSession(
   callback: (id: number | undefined) => void,
 ) {
   const db = GetDatabase();
-  const tableName = 'shootsessions';
 
   db.transaction(tx => {
     tx.executeSql(
-      `CREATE TABLE IF NOT EXISTS ${tableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, bow TEXT, note TEXT)`,
+      `CREATE TABLE IF NOT EXISTS ${shootSessionTableName} (id INTEGER PRIMARY KEY AUTOINCREMENT, bow TEXT, note TEXT)`,
     );
   });
 
   db.transaction(tx => {
     tx.executeSql(
-      `INSERT INTO ${tableName} (bow, note) VALUES(?, ?)`,
+      `INSERT INTO ${shootSessionTableName} (bow, note) VALUES(?, ?)`,
       [session.bow.type, session.note],
       (_, resultSet) => {
-        console.log(resultSet);
         callback(resultSet.insertId);
       },
       (_, error) => {
         console.warn(error);
         callback(undefined);
+        return false;
+      },
+    );
+  });
+}
+
+/*
+Return all sessions
+*/
+export function GetSessions(callback: (session: ShootSession[]) => void) {
+  const db = GetDatabase();
+
+  db.transaction(tx => {
+    tx.executeSql(
+      `SELECT * FROM ${shootSessionTableName}`,
+      [],
+      (_, resultSet) => {
+        callback(resultSet.rows._array);
+      },
+      (_, error) => {
+        console.log(error);
         return false;
       },
     );
