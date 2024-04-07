c050651b265320fb115ec42a970fe6aa60116417
Zeeba <Zeeba@STARK REBORN>
2024-04-07 20:06:38 +0100

    Flex rows

--- src/Components/CustomCard.tsx
+++ src/Components/CustomCard.tsx
@@ -14,12 +14,13 @@ export default function CustomCard({
 }: CustomCardProps) {
   return (
     <View style={globalStyles.pageContainer}>
-      <View style={globalStyles.container}>
+      <View
+        style={[globalStyles.container, (style = { flexDirection: 'row' })]}>
         <Image
           id="image-thumbnail"
           style={styles.thumbnail}
           source={{
-            uri: 'https://en.wikipedia.org/wiki/Patrick_Stewart#/media/File:Patrick_Stewart_by_Gage_Skidmore_2.jpg',
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Patrick_Stewart_by_Gage_Skidmore_2.jpg',
           }}
         />
         <View id="middle-content">
@@ -35,11 +36,13 @@ export default function CustomCard({
 const styles = StyleSheet.create({
   cardHeading: {
     fontWeight: 'bold',
+    flex: 0.5,
   },
 
   thumbnail: {
     flex: 0.3,
-    width: 100,
-    height: 100,
+    width: 50,
+    height: 50,
+    borderRadius: 40,
   },
 });
