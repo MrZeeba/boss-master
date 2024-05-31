a174229c5842817f8044a0910b502b4d3aa3868e
Zeeba <Zeeba@STARK REBORN>
2024-05-31 08:40:44 +0100

    Add custom button specifically for scores

--- src/Components/ScoreSelector.tsx
+++ src/Components/ScoreSelector.tsx
@@ -1,6 +1,7 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { ScoringType } from '../Enums/ScoringType';
-import CustomButton from './CustomButton';
+import { globalStyles } from '../globalStyles';
 
 interface ScoreSelectorProps {
   mode: ScoringType;
@@ -8,10 +9,43 @@ interface ScoreSelectorProps {
 
 export default function ScoreSelector({ mode }: ScoreSelectorProps) {
   return (
-    <View>
-      <CustomButton title="X" onPress={() => console.log('x was pressed')} />
-      <CustomButton title="9" onPress={() => console.log('9 was pressed')} />
-      <CustomButton title="8" onPress={() => console.log('8 was pressed')} />
+    <View style={styles.row}>
+      <ScoreButton text="X" colour="yellow" />
+      <ScoreButton text="9" colour="yellow" />
+      <ScoreButton text="8" colour="red" />
+      <ScoreButton text="7" colour="red" />
+      <ScoreButton text="6" colour="lightblue" />
+      <ScoreButton text="5" colour="lightblue" />
+      <ScoreButton text="4" colour="black" />
+      <ScoreButton text="3" colour="black" />
+      <ScoreButton text="2" colour="white" />
+      <ScoreButton text="1" colour="white" />
+      <ScoreButton text="M" colour="orange" />
     </View>
   );
 }
+
+function ScoreButton({ text, colour }) {
+  return (
+    <TouchableOpacity
+      style={[
+        globalStyles.button,
+        styles.scoreButton,
+        { backgroundColor: colour },
+      ]}
+      onPressOut={() => console.log(`${text} was pressed`)}>
+      <Text>X</Text>
+    </TouchableOpacity>
+  );
+}
+
+export const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  scoreButton: {
+    margin: 2,
+  },
+});
